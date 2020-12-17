56dd5f3cc8eb4a14cf167e5476f7517f65851039
Zahnen Garner <zahnen@gmail.com>
2020-12-17 11:33:14 -0800

    added test for Season constructor
    Co-authored-by: Taylor Delph <taylulzcode@gmail.com>
    Co-authored-by: Svea Wade <svealinneawade@gmail.com>

--- TravelDiary.Tests/ModelTests/SeasonTests.cs
+++ TravelDiary.Tests/ModelTests/SeasonTests.cs
@@ -1,4 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ToDoList.Models;
+using TravelDiary.Models;
 using System.Collections.Generic;
 using System;
+
+namespace TravelDiary.Tests
+{
+  [TestClass]
+  public class SeasonTest
+  {
+    [TestMethod]
+    public void SeasonConstructor_CreatesAnInstanceOfSeason_Season()
+    {
+      Season newSeason = new Season("test string");
+      Assert.AreEqual(typeof(Season), newSeason.GetType());
+    }
+  }
+}
+
